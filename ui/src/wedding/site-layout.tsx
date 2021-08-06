ee7f0f8108419cbbeaef18b389604a31de59b7bc
Ike Wilson <iwilson@ecfmg.org>
2021-08-06 11:15:19 -0400

    Alice background photo

--- ui/src/wedding/site-layout.tsx
+++ ui/src/wedding/site-layout.tsx
@@ -4,6 +4,7 @@ import { Layout, Menu } from 'antd';
 import { Footer, Content, Header } from 'antd/lib/layout/layout';
 import Main from './main';
 import ContactUs from './contact-us';
+import { HomeFilled, MailFilled } from '@ant-design/icons';
 
 const SiteLayout: FC<RouteComponentProps> = (props) => {
   const [currentPage, setCurrentPage] = useState<string>('1');
@@ -11,9 +12,15 @@ const SiteLayout: FC<RouteComponentProps> = (props) => {
   return (
     <Layout style={{height:"100vh"}}>
       <Header className={'header'} style={{height: '3%'}}>
-        <span className={'link'} onClick={() => setCurrentPage('1')}>Home</span>
+        <span className={'hover'} onClick={() => setCurrentPage('1')}>
+          <span className={'link-image'}><HomeFilled /></span>{" "}
+          <span className={'link'}>Home</span>
+        </span>
         {" | "}
-        <span className={'link'} onClick={() => setCurrentPage('2')}>Contact Us</span>
+        <span className={'hover'} onClick={() => setCurrentPage('2')}>
+          <span className={'link-image'}><MailFilled /></span>{" "}
+          <span className={'link'}>Contact Us</span>
+        </span>
       </Header>
       <Content style={{height: '94%'}}>
         {currentPage === '1' && <Main />}
